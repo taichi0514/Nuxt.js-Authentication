c5bb6bd5ceec2228a617e2dade2d98933359317b
taichi.murai <taichimurai0514@gmail.com>
2019-03-26 01:40:22 +0900

    fi 修正

--- middleware/mypage.js
+++ middleware/mypage.js
@@ -6,11 +6,8 @@ export default function ({route, redirect, store}) {
     if (Notification.permission !== "denied") {
       Notification.requestPermission(function (permission) {
       });
-      // Retrieve Firebase Messaging object.
-      const messaging = firebase.messaging();
-      messaging.usePublicVapidKey("BK05JP91BVFnCHgDYRM-q0I7dqgCwyTlFs2k4Z152HHU2Ben9NfuZjz9duR2y7TSBfJh1r7Im2FOdT-7i8SXy34");
-
     }
+  }
 
 
     notifyMe();
